8463915490fb8da43f1c75efc38a5260a1e4061f
Afushu <135005337+Afushu@users.noreply.github.com>
2026-04-24 21:00:44 +0800

    Update MobileActivity to load MobileHomeFragment

--- app/src/main/java/com/emby/client/ui/mobile/MobileActivity.kt
+++ app/src/main/java/com/emby/client/ui/mobile/MobileActivity.kt
@@ -2,10 +2,18 @@ package com.emby.client.ui.mobile
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentTransaction
 
 class MobileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mobile)
+
+        if (savedInstanceState == null) {
+            val fragment = MobileHomeFragment()
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit()
+        }
     }
 }
